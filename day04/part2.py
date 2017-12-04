73616fb3147a8ad5f4e2bd7026d07721816230a9
Bennett Hreherchuk <hreherch@ualberta.ca>
2017-12-04 00:03:44 -0700

    create day04: part2.py

---
+++ day04/part2.py
@@ -0,0 +1,21 @@
+import sys
+
+filename = sys.argv[1]
+file = open(filename, 'r')
+
+sum = 0
+
+for line in file:
+    passes = True
+    passDict = {}
+    for word in line.split():
+        word = "".join(sorted(word))
+        if word in passDict:
+            passes = False
+        else:
+            passDict[word] = True
+
+    if passes:
+        sum += 1
+
+print(sum)

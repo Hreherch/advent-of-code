fd628481941564f37fcd4661228c8f3c602dd7a8
Bennett Hreherchuk <hreherch@ualberta.ca>
2017-12-05 22:16:58 -0700

    create day06: part2.py

---
+++ day06/part2.py
@@ -0,0 +1,38 @@
+import sys
+
+filename = sys.argv[1]
+file = open(filename, 'r')
+
+memoryDict = {}
+banks = []
+
+for line in file:
+    for blocks in line.split():
+        banks.append(int(blocks))
+
+memoryDict[tuple(banks)] = True
+
+cycles = 0
+start_t = 0
+while True:
+    maxValue = max(banks)
+    index = banks.index(maxValue)
+
+    banks[index] = 0
+
+    for count in range(maxValue):
+        index = (index+1)%len(banks)
+        banks[index] += 1
+
+    cycles += 1
+
+    bank_t = tuple(banks)
+    if bank_t in memoryDict:
+        if start_t == 0:
+            start_t = bank_t
+            cycles = 0
+        elif start_t == bank_t:
+            print(cycles)
+            break
+    else:
+        memoryDict[bank_t] = True
